8df331f8a6374a3af91d4f243d72371e169d6ffd
Swetaketu Majumder <swetaketum@gmail.com>
2018-01-30 05:19:42 +0100

    Sign In API called, alerts shown

--- foQus/ViewController.swift
+++ foQus/ViewController.swift
@@ -22,11 +22,71 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
     @IBAction func ButtonClick(_ sender: Any) {
-        let alertController = UIAlertController(title: "Sign In", message:
-            "Username : \(username.text!)\nPassword : \(password.text!)", preferredStyle: UIAlertControllerStyle.alert)
-        alertController.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.default,handler: nil))
         
-        self.present(alertController, animated: true, completion: nil)    }
+        let configuration = URLSessionConfiguration.default
+        let session = URLSession(configuration: configuration)
+        
+        let params = ["username":username.text, "password":password.text] as Dictionary<String, AnyObject>
+        
+        let urlString = NSString(format: "http://optystech.com/rest/rest-auth/login/");
+        print("url string is \(urlString)")
+        let request : NSMutableURLRequest = NSMutableURLRequest()
+        request.url = URL(string: ((((NSString(format: "%@", urlString)as String) as String) as String) as String) as String)
+        request.httpMethod = "POST"
+        request.timeoutInterval = 30
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        request.httpBody  = try! JSONSerialization.data(withJSONObject: params, options: [])
+        print(request.httpBody as Any);
+        let dataTask = session.dataTask(with: request as URLRequest)
+        {
+            ( data: Data?, response: URLResponse?, error: Error?) -> Void in
+            // 1: Check HTTP Response for successful request
+            guard let httpResponse = response as? HTTPURLResponse, let receivedData = data
+                else {
+                    print("error: not a valid http response")
+                    return
+            }
+            
+            switch (httpResponse.statusCode)
+            {
+            case 200:
+                
+                let resp = NSString (data: receivedData, encoding: String.Encoding.utf8.rawValue)
+                print(resp as Any)
+                
+                if (resp != nil)
+                {
+                    print(resp as Any)
+                    DispatchQueue.main.async {
+                        let alertController = UIAlertController(title: "Sign In", message:
+                            "Success", preferredStyle: UIAlertControllerStyle.alert)
+                        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default,handler: nil))
+                        self.present(alertController, animated: true, completion: nil)
+                    }
+                }
+                else
+                {
+                    DispatchQueue.main.async {
+                        let alertController = UIAlertController(title: "Sign In", message:
+                            "Error", preferredStyle: UIAlertControllerStyle.alert)
+                        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default,handler: nil))
+                        self.present(alertController, animated: true, completion: nil)
+                    }
+                }
+                
+            default:
+                DispatchQueue.main.async {
+                    let alertController = UIAlertController(title: "Sign In", message:
+                        "Invalid Credentials", preferredStyle: UIAlertControllerStyle.alert)
+                    alertController.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default,handler: nil))
+                    self.present(alertController, animated: true, completion: nil)
+                }
+            }
+        }
+        dataTask.resume()
+    }
 }
 
